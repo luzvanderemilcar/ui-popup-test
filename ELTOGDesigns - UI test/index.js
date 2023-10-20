1268e231a4033577e0e2d724b75b14210d01fcd2
emiltog@gmail.com <emiltog@gmail.com>
2023-10-19 20:40:23 -0400

    Add click effect to pop up clickable elements

--- ELTOGDesigns - UI test/index.js
+++ ELTOGDesigns - UI test/index.js
@@ -1,23 +1,37 @@
 // DOM Elements 
 $closePopUpViewButton = $(".close-button");
 $expandPopUpViewButton = $(".expand-view-button");
+$primaryCta = $(".primary-cta");
+$secondaryCta = $(".secondary-cta")
+
 
 // Event Listeners 
 $closePopUpViewButton.on("click", closePopUpView);
+$expandPopUpViewButton.on("click", expandPopUpView);
+$primaryCta.on("click", orderArticle);
+$secondaryCta.on("click", exploreItem);
 
-
-function closePopUpView(e) {
-    applyClickEffectToElement(e.target.class());
+function closePopUpView() {
+    applyClickEffectToElement($(this));
 }
 
+function expandPopUpView() {
+    applyClickEffectToElement($(this));
+}
 
-function applyClickEffectToElement(classList) {
-    let elem = $("." + classList);
+function orderArticle() {
+    applyClickEffectToElement($(this))
+}
+
+function exploreItem() {
+    applyClickEffectToElement($(this))
+}
+
+function applyClickEffectToElement(elem) {
     elem.toggleClass("clicked");
     let clickTimeout = setTimeout(()=>{
        elem.toggleClass("clicked");
         clearTimeout(clickTimeout);
         clickTimeout = null;
-    }, 300)
-    
+    }, 200)
 }
