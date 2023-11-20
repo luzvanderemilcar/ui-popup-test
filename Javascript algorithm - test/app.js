05093cc560eea6438f91887d7f2515607ce01c88
emiltog@gmail <emiltog@gmail>
2023-11-20 12:23:21 -0500

    update test algorithm for Calculator.  Bug stop the development, so I'll try another dev environment before continuing.

--- Javascript algorithm - test/app.js
+++ Javascript algorithm - test/app.js
@@ -1,7 +1,6 @@
-let regex  = /([+\-×÷]{1,})(?=([+×÷][\-]{0,1}[0-9.]+))/g;
- //regex = /\d+/g;
-let testSting = "23÷++-5÷+××-3";
-console.log(testSting.match(regex));
+//Regex to test the input
+const operatorRegex = /[+\-×÷]/g;
+const numberRegex = /[0-9]/g;
 
 function signCleanUp(str) {
     //--|++ => + change 2 minus or 2 plus into 1 plus
@@ -14,4 +13,53 @@ function signCleanUp(str) {
     return str2.replace(signCleanUpRegex, "");
 }
 
-console.log(signCleanUp (testSting));
+function inputCheck(str, data) {
+    let result;
+    // Input a sign right after another 
+    if (operatorRegex.test(str[str.length - 1]) && operatorRegex.test(data)) {
+        if (data = "-") {
+            result = str + data;
+            console.log(result)
+        } else {
+        result = str.slice(0, str.length - 1) + data;
+        }
+    }
+   else if (str == "0") {
+        if (data == "." || operatorRegex.test(data)) {
+            result = str + data
+
+        }
+        else if (numberRegex.test(data)) {
+            result = data;
+        }
+    }
+    
+    else if (str != "0") {
+        // allow only one decimal point for a number
+        if (data == "." && !haveDecimalPoint(str)) {
+            result = str + data;
+        }
+
+       /* else if (numberRegex.test(data) || operatorRegex.test(data)) {
+            result = str + data;
+
+        }*/
+    }
+    return result;
+}
+
+let stringProcess = "+";
+let dataProcess = "+";
+
+//console.log(inputCheck(stringProcess, dataProcess))
+console.log(operatorRegex.test(stringProcess));
+console.log(operatorRegex.test(dataProcess));
+// There is a bug here, I test the same thing with two different result
+
+console.log(operatorRegex.test(dataProcess));
+console.log(operatorRegex.test(stringProcess));
+
+function haveDecimalPoint(str) {
+    let arrNumbers = str.split(operatorRegex);
+    return /[.]/.test(arrNumbers[arrNumbers.length - 1]);
+}
